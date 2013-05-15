cd4e1566511fd8763cef407d2e28bf5ef09a7df3
Timothee Cour <timothee.cour2@gmail.com>
2013-05-15 13:31:02 -0700

    _

--- dtools/util/util.d
+++ dtools/util/util.d
@@ -13,3 +13,37 @@ unittest{
 	auto a=make!(A,double)(1);
 	static assert(is(typeof(a) == A));
 }
+
+// Take address of a class/struct/anything
+void*AddressOf(T)(T a) if(is(T==class)){
+	return *cast(void**)& a;
+}
+T*AddressOf(T)(ref T a) if(!is(T==class)){
+	return &a;
+}
+
+unittest{
+	import std.stdio;
+	import std.conv;
+
+	class A{
+		int x;
+		//	T opCast(T : int)() { return 1; }
+		int opCast(T:void*)() { return 1; }
+		//	auto opCast(T:void*)() { return null; }
+	}
+	struct B{		
+	}
+
+	A a;
+	assert(AddressOf(a) is null);
+	a = new A;
+	auto a2=a;
+	auto pa=AddressOf(a);
+	assert(AddressOf(a) == AddressOf(a2) );
+	B b;
+	assert(AddressOf(b) !is null);
+	B*b2;
+	assert(AddressOf(*b2) is null);
+	//	writeln(AddressOf(B.init)); //waiting for rvalue ref, DIP39
+}
