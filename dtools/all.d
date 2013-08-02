bf23e4564afd736e2a270a095342a39f4406feab
Timothee Cour <timothee.cour2@gmail.com>
2013-08-02 15:59:30 -0700

    _

--- dtools/all.d
+++ dtools/all.d
@@ -6,7 +6,6 @@ module dtools.all;
 
 public import dtools.util.d_funs;
 public import dtools.util.functional;
-public import dtools.util.log;
 public import dtools.util.util;
 public import dtools.util.versions;
 public import dtools.util.cast_funs;
--- dtools/examples/test1.d
+++ dtools/examples/test1.d
@@ -11,33 +11,6 @@ void main(){
 }
 
 version(test1){
-	import dtools.util.log;
-
-	void test(){
-		double x=1.1;
-		auto s="abc";
-		mixin("x*2=$(x*2), string=$s...".parse).writeln; //s1="x=1.1, string=abc"
-		format("x*2=%s, string=%s...",x*2,s).writeln;
-		
-		version(none){
-			string sa=mixin("@(x*2), @s...".parse); //s1="x=1.1, string=abc"
-			string sc=mixin(prettyPrint(q{x*2,s}));
-			string sc=mixin(prettyPrint(x*2,s));
-			
-			string sc=prettyPrint(x*2,s);
-			
-			version(none) //could be this simple with @mixin construct
-				string sc=prettyPrint(x*2,s);
-			
-			//example from std.datetime:
-			string s1a=mixin(`_assertPred!"$op" failed: [$lhs] is not %op [%rhs].`.parse);
-			string s1b=format(`_assertPred!"%s" failed: [%s] is not %s [%s].`, op, lhs, op, rhs);
-			
-			//example from std.net.isemail
-			string s2a=mixin("EmailStatus\n{\n\tvalid: $valid\n\tlocalPart: $localPart\n\tdomainPart: $domainPart\n\tstatusCode: $statusCode\n}".parse);
-			string s2b=format("EmailStatus\n{\n\tvalid: %s\n\tlocalPart: %s\n\tdomainPart: %s\n\tstatusCode: %s\n}", valid,localPart, domainPart, statusCode);
-		}
-	}
 }
 else
 version(test2){
